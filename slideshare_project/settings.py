d6114f163d9d11678d912787fc87f5b8a7681cc5
Muhammad Ameer Hamza <129995036+nexusameer@users.noreply.github.com>
2025-01-17 01:28:06 +0500

    heroku settings

--- slideshare_project/settings.py
+++ slideshare_project/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-sy^8!dlpus3)7!b&+wn9$y1($-4i%gwv&t#)02b5!#gt_ddu&e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
