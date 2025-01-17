13c7694d120ee716f42c7f03d405684f09ddf4b0
Muhammad Ameer Hamza <129995036+nexusameer@users.noreply.github.com>
2025-01-17 16:20:10 +0500

    dsf

--- slideshare_api/urls.py
+++ slideshare_api/urls.py
@@ -1,6 +1,7 @@
 # slideshare_api/urls.py
 from django.urls import path
 from .views import SlideShareDownloadView
+from . import views
 
 urlpatterns = [
     path('download/', SlideShareDownloadView.as_view(), name='download_slideshare'),
